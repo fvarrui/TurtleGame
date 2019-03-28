cda60cbc5d14a0517b151831c7822d6a8ca0f4cd
Fran.vargas(ruiz) <4354486+fvarrui@users.noreply.github.com>
2019-03-28 12:50:15 +0000

    U

--- src/main/java/fvarrui/games/turtlegame/Turtle.java
+++ src/main/java/fvarrui/games/turtlegame/Turtle.java
@@ -9,7 +9,7 @@ import fvarrui.games.turtlegame.controller.Gamepad;
 import fvarrui.games.turtlegame.controller.PS4GamepadCodes;
 
 public class Turtle extends TexturedActor {
-	
+
 	public Turtle() {
 		super(new Texture(Gdx.files.internal("assets/turtle-1.png")));
 	}
@@ -17,16 +17,16 @@ public class Turtle extends TexturedActor {
 	@Override
 	public void act(float delta) {
 		super.act(delta);
-		
+
 		float velocity = 0f;
 		float rotation = getRotation();
-		
+
 		// check keyboard input
 		if (Gdx.input.isKeyPressed(Keys.UP)) {
 			velocity = 8.0f;
 		}
 		if (Gdx.input.isKeyPressed(Keys.DOWN)) {
-			velocity = -8.0f; 
+			velocity = -8.0f;
 			rotation += 180;
 		}
 		if (Gdx.input.isKeyPressed(Keys.LEFT)) {
@@ -35,26 +35,28 @@ public class Turtle extends TexturedActor {
 		if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
 			rotateBy(-5);
 		}
-		
+
 		// check gamepad input
-		float leftX = Gamepad.getGamepad().getAxis(PS4GamepadCodes.AXIS_LEFT_X);
-		float leftY = Gamepad.getGamepad().getAxis(PS4GamepadCodes.AXIS_LEFT_Y);
-		if (leftY > 0.1) {
-			velocity = 8 * leftY;
-			rotation += 180;			
+		if (Gamepad.getGamepad() != null) {
+			float leftX = Gamepad.getGamepad().getAxis(PS4GamepadCodes.AXIS_LEFT_X);
+			float leftY = Gamepad.getGamepad().getAxis(PS4GamepadCodes.AXIS_LEFT_Y);
+			if (leftY > 0.1) {
+				velocity = 8 * leftY;
+				rotation += 180;
+			}
+			if (leftY < -0.1) {
+				velocity = 8 * leftY;
+			}
+			if (leftX > 0.1 || leftX < -0.1) {
+				rotateBy(5 * -leftX);
+			}
 		}
-		if (leftY < -0.1) {
-			velocity = 8 * leftY; 
-		}
-		if (leftX > 0.1 || leftX < -0.1) {
-			rotateBy(5 * -leftX);
-		}
-		
+
 		Vector2 direction = new Vector2(Vector2.X);
 		direction.setLength(velocity);
 		direction.setAngle(rotation);
 
-		moveBy(direction.x, direction.y); 
+		moveBy(direction.x, direction.y);
 	}
 
 }
